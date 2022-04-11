a99ed14f517c3b33840895235f70ea13a115a287
lele71 <heidix.dor@gmail.com>
2022-04-11 14:22:00 +0200

    first commit

---
+++ logger.py
@@ -0,0 +1,143 @@
+class Logger:
+    def __init__(self, position, appName, level='noset'):
+        self.position = position
+        self.appName = appName
+        self.currentFilter=[0]
+        self.lastError = ''
+        self.availableFilter = {
+            0: 'NOSET',
+            10: 'DEBUG',
+            20: 'INFO',
+            30: 'WARNING',
+            40: 'ERROR',
+            50: 'CRITICAL',
+            100: 'TOSTDOUT'
+        }
+    
+    # private get datetine like string
+    def __datetime(self):
+        from datetime import datetime
+        now = datetime.now()
+        nowString = now.strftime("%y-%m-%d %H:%M:%s")
+        return nowString
+    # private method to write to file or stdout
+    def __writeLog(self, message):
+        print(message)
+        
+    # Return the last error    
+    def getLastError(self):
+        return self.lastError
+    
+    # Return list of filters
+    def getFilter(self):
+        return self.currentFilter
+    
+    # return available filter
+    def getAvailableFilter(self):
+        return self.availableFilter
+    
+    # Add a filter ti list if not present
+    def addFilter(self, filterList):
+        error = False
+        for f in filterList:
+            if type(f) == int:
+                # is int
+                # check if filter exist
+                if f in self.availableFilter:
+                    # check if filter is not already in list
+                    if not f in self.currentFilter:
+                        # not il list append
+                        self.currentFilter.append(f)
+                    else:
+                        # already in list error
+                        self.lastError += "%s already in list\n"%str(f)
+                        error = True
+                else:
+                    # not in available filter error
+                    self.lastError += "%s not in available filter\n"%str(f)
+                    error = True
+            elif type(f) == str:
+                # is str
+                # check filter code
+                filterExist = False
+                for j in self.availableFilter:
+                    # check if in available filter
+                    if f.upper() == self.availableFilter[j]:
+                        filterExist = True
+                        # check if filter is not already in list
+                        if not f in self.currentFilter:
+                            # not il list append
+                            self.currentFilter.append(j)
+                        else:
+                            # already in list error
+                            self.lastError += "%s already in list\n"%str(f)
+                            error = True
+                if filterExist:
+                    error = False
+                else:  
+                    # not in available filter error
+                    self.lastError += "%s not in available filter\n"%str(f)              
+            else:
+                # not int or str error
+                self.lastError += "%s not correct format\n"%str(f)
+                error = True
+        return not error
+    
+    def log(self, level, msg, timestamp="None"):
+        error = False
+        # check if log is filtred
+        # case level is int
+        if type(level) == int:
+            # int provided
+            # check if level exist
+            if level in self.availableFilter:
+                # check if level is filtered
+                if level not in self.currentFilter:
+                    # level not filtered write log
+                    levelTxt = self.availableFilter[level]
+                    # check if timestamp is provided
+                    if timestamp != "None":
+                        now = timestamp
+                    else:
+                        now = self.__datetime()
+                    message = "%s from %s <%s> %s"%(now, self.appName, levelTxt, msg)
+                    self.__writeLog(message)    
+                else:
+                    # log filtered for now only pass
+                    pass
+            else:
+                # not in available filter error
+                self.lastError += "%s not in available filter\n"%str(level)
+                error = True 
+        elif type(level) ==  str:
+            filterExist = False
+            for j in self.availableFilter:
+                # check if in available filter
+                if level.upper() == self.availableFilter[j]:
+                    # exist in available filter
+                    filterExist = True
+                    print("TROVATO")
+                    # check if is filtered
+                    if level not in self.currentFilter:
+                        # level not filtered write log
+                        # check if timestamp is provided
+                        if timestamp != "None":
+                            now = timestamp
+                        else:
+                            now = self.__datetime()
+                        message = "%s from %s <%s> %s"%(now, self.appName, level.upper(), msg)
+                        self.__writeLog(message)
+                    else:
+                        # log filtered for now only pass
+                        pass 
+            if filterExist:
+                error = False
+            else:  
+                # not in available filter error
+                self.lastError += "%s not in available filter\n"%str(level)
+                error = True
+        else:
+            # no int or str provided
+            self.lastError += "%s not correct format\n"%str(level)
+            error = True
+        return not error
---
+++ test.py
@@ -0,0 +1,56 @@
+from logger import Logger
+
+def main():
+    myLog = Logger("position", "test")
+    
+    '''
+    # Test getLastError
+    print("Ultimo errore: %s"%myLog.getLastError())
+    print("----------------------------------------------------------------")
+    # Test getLevel
+    print("Livelli filtro: %s"%str(myLog.getFilter()))
+    print("----------------------------------------------------------------")
+    # Test getAvailableFilter
+    print("Filtri disponibiuli: %s"%str(myLog.getAvailableFilter()))
+    print("----------------------------------------------------------------")
+    # Test addFilter
+    print("Aggiungo filtri corretti:\n")
+    if myLog.addFilter([20,"WARNING"]):
+        print("Filtri aggiunti correttamente")
+        print("Livelli filtro: %s"%str(myLog.getFilter()))
+        print("----------------------------------------------------------------")
+    else:
+        print("Errore aggiunta filtri")
+        print(myLog.getLastError())
+        print("----------------------------------------------------------------")
+    print("Aggiungo filtri errati interi:\n")
+    if myLog.addFilter([20,0,25,[12]]):
+        print("Filtri aggiunti correttamente")
+        print("Livelli filtro: %s"%str(myLog.getFilter()))
+        print("----------------------------------------------------------------")
+    else:
+        print("Errore aggiunta filtri")
+        print(myLog.getLastError())
+        print("----------------------------------------------------------------")
+    print("Loggo ok:\n")
+    if myLog.log(20,"messaggio"):
+        print("log ok")
+        print("----------------------------------------------------------------")
+    else:
+        print("Errore log")
+        print(myLog.getLastError())
+        print("----------------------------------------------------------------")
+    print("Loggo errore:\n")
+    '''
+    myLog.addFilter([20])
+    if myLog.log(30,"messaggio"):
+        print("log ok")
+        print("----------------------------------------------------------------")
+    else:
+        print("Errore log")
+        print(myLog.getLastError())
+        print("----------------------------------------------------------------")
+
+
+if __name__ == '__main__':
+    main()
